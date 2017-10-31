21112b86afeab41d7f685990b7f5641db5e9001e
ashutosh pawar <ashutoshpawar07@gmail.com>
2017-10-31 19:05:35 +0530

    added views comment

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+# this is the new comment added from mac lets see if it works
 
 def index(request):
     return HttpResponse('hello world')
