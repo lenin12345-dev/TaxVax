01defabc3078ba809fd146e5c9a9d61b9939fe88
Ashu <ashutoshpawar07@gmail.com>
2017-10-31 19:16:33 +0530

    added windows comment

--- home/views.py
+++ home/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 # this is the new comment added from mac lets see if it works
-
+# this is the comment added on windows
 def index(request):
     return HttpResponse('hello world')
